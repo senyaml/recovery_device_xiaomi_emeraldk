cc7a506a43ed4c403678c5c129413e5e86990506
alterego <125763784+senyaml@users.noreply.github.com>
2024-07-07 07:19:57 +0700

    fix gflags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,6 +17,10 @@
 #
 # 	Please maintain this if you use this script or any part of it
 #
+
+# Clone to fix build on minimal manifest
+git clone https://android.googlesource.com/platform/external/gflags/ -b android-12.1.0_r4 external/gflags
+
 FDEVICE="emerald"
 #set -o xtrace
 
